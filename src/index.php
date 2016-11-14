506495ae4724228c18cbeb7e27b5384888dab9b0
Mikhail Semin <bifot@bifot.ru>
2016-11-14 19:36:17 +0300

    Fixed error-url.

--- src/index.php
+++ src/index.php
@@ -1,54 +1,54 @@
 <?php include 'header.php'; ?>
 <h1 class="main_title">Select a team</h1>
 <div class="all_list_flex">
-	<div class="team_name">
-		<a href="astralis"><img alt="" src="img/astralis.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="clg"><img alt="" src="img/clg.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="dignitas"><img alt="" src="img/dignitas.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="envuys"><img alt="" src="img/envyus.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="f3"><img alt="" src="img/f3.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="faze"><img alt="" src="img/faze.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="fnatic"><img alt="" src="img/fnatic.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="g2"><img alt="" src="img/g2.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="gambit"><img alt="" src="img/gambit.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="liquid"><img alt="" src="img/liquid.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="mouz"><img alt="" src="img/mouz.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="navi"><img alt="" src="img/navi.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="nip"><img alt="" src="img/nip.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="optic"><img alt="" src="img/optic.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="sk"><img alt="" src="img/sk.png"></a>
-	</div>
-	<div class="team_name">
-		<a href="virtuspro"><img alt="" src="img/vp.png"></a>
-	</div>
+  <div class="team_name">
+    <a href="astralis"><img alt="" src="img/astralis.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="clg"><img alt="" src="img/clg.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="dignitas"><img alt="" src="img/dignitas.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="envyus"><img alt="" src="img/envyus.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="f3"><img alt="" src="img/f3.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="faze"><img alt="" src="img/faze.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="fnatic"><img alt="" src="img/fnatic.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="g2"><img alt="" src="img/g2.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="gambit"><img alt="" src="img/gambit.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="liquid"><img alt="" src="img/liquid.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="mouz"><img alt="" src="img/mouz.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="navi"><img alt="" src="img/navi.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="nip"><img alt="" src="img/nip.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="optic"><img alt="" src="img/optic.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="sk"><img alt="" src="img/sk.png"></a>
+  </div>
+  <div class="team_name">
+    <a href="virtuspro"><img alt="" src="img/vp.png"></a>
+  </div>
 </div>
 
 <?php include 'footer.php'; ?>
